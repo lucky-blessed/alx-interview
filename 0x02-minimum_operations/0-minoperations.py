789e8562b4b08edc0ba991d86a728df6b01ae098
lucky-blessed <blessednkwor7@gmail.com>
2024-07-11 10:50:42 +0100

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def minOperations(n):
+    """
+    To calculate the fewest number of operations needed to result in
+    exactly n 'H' characters in the file.
+
+    Args:
+        n: The target number of H characters.
+
+
+    Returns:
+        The minimum number of operation needed, or 0 if n cannot be achieved
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    current_length = 1
+    clipboard = 0
+
+    while current_length < n:
+        if n % current_length == 0:
+            clipboard = current_length
+            operations += 1 # Copy All
+        current_length += clipboard
+        operations += 1 # Paste
+
+    return operations
